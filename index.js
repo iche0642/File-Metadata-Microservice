1b5ea61cea6c52d7c1f1ceb664456791a156d774
iche0642 <isaac.cheang267@gmail.com>
2025-10-24 21:59:43 +1100

    Add multer file upload functionality

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
 var express = require('express');
 var cors = require('cors');
+var multer = require('multer');
 require('dotenv').config()
 
 var app = express();
+
+var upload = multer({ storage: multer.memoryStorage() });
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
+});
+
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
+
+  if (!req.file) {
+    return res.json({ error: 'No file uploaded' });
+  }
+  
+  // Return file metadata
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  });
 });
 
 
